44111c8b7f17beb99edc1072c57897da230f83b9
use04118 <use04118@gmail.com>
2025-05-15 16:36:00 +0530

    Update settings.py

--- mybillbook/mybillbook/mybillbook/settings.py
+++ mybillbook/mybillbook/mybillbook/settings.py
@@ -170,7 +170,7 @@ REST_FRAMEWORK = {
     
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,  # Limit results per page to 10
+    'PAGE_SIZE': 10,  # Limit results per page to 10
     
 
 }
