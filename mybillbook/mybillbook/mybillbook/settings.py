9efe0baedb7cc2748c565a721cb83ff92804baff
use04118 <use04118@gmail.com>
2025-05-20 13:14:22 +0530

    Update settings.py

--- mybillbook/mybillbook/mybillbook/settings.py
+++ mybillbook/mybillbook/mybillbook/settings.py
@@ -187,7 +187,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://192.168.1.3:5173",  # React app
     "http://192.168.1.35:5173",  # React app
-    "https://kaleidoscopic-frangollo-146587.netlify.app",
+    "https://stupendous-kleicha-92b259.netlify.app/",
 ]
 
 
