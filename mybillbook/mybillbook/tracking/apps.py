a600270ba6baf45c78e9d2ab3a2bb9729f99f6ba
use04118 <use04118@gmail.com>
2025-05-28 13:17:20 +0530

    Delete mybillbook/mybillbook/tracking directory

--- mybillbook/mybillbook/tracking/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrackingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tracking'
-
-    def ready(self):
-        import tracking.signals  # Import signals when app is ready
